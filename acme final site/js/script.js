0cc26fb359dd751f12334fd3beb287a0345d9ec1
nlapi <nisjet.lapi@gmail.com>
2017-04-01 17:52:13 -0600

    Add files via upload

---
+++ acme final site/js/script.js
@@ -0,0 +1,63 @@
+$(function () {
+
+    $.getJSON("./js/acme.json", function (data) {
+        console.log(data);
+
+        var output = '<li><a href="https://nlapi.github.io">Home</a></li>';
+        var counter = 0;
+
+        $.each(data.Navigation, function() {
+            output += '<li>';
+            output += '<a href=#' + counter + '>' + data.Navigation[counter] + '</a>';
+            output += '</li>';
+            counter++;
+        });
+
+        $("#navbar").html(output);
+    }); // end getJSON
+
+});
+
+$( "#navbar" ).on( "click", "a", function(ev) {
+
+    var item = $(this).text();
+
+    if( item == "Home") {
+        makeHomePage();
+    } else {
+        getData(item);
+    }
+});
+
+function getData(item){
+
+    console.log(item);
+
+    $.ajax({
+        url : "./js/acme.json",
+        dataType : "json",
+        success : function(data) {
+            console.log(data[item].name);
+            $("#zhvillim-produkti").css("display", "flex");
+            $("#emri-produktit").text(data[item].name);
+            $("#pershkrimi-produktit").css("background-image", "url(" + data[item].path + ")");
+            $("#ne-shtepi").css("display", "none");
+			
+            var output = '';
+
+            output += "<li>" + data[item].description + "</li>"
+            output += '<li><strong>Made by: </strong>' + data[item].manufacturer + '</li>';
+            output += '<li><strong>Reviews: </strong>' + data[item].reviews + '/5 stars</li>';
+            output += '<li><h2>Price: $' + data[item].price + '</h2></li>';
+
+            $("#pershkrimi-produktit").html(output);
+
+            $("#pershkrimi-produktit h2").css("color", "#de2226");
+        }
+    });
+}
+
+function makeHomePage() {
+    $("#ne-shtepi").css("display", "flex");
+    $("#zhvillim-produkti").css("display", "none");
+}
